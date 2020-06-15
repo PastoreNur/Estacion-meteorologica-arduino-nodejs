a96f54a931e3e1a8bc259db1d506c8ed96c2fadc
Julio Eduardo Pastore Baires <julio.pastore.jp@gmail.com>
2020-06-15 10:13:17 -0600

    json
    json controller para "BD" listo

--- jsoncontroller.js
+++ jsoncontroller.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const entradas = require('./entradas.json')
+const { time } = require('console')
 function escribir(params) {
     entradas.push(params)
 
@@ -8,11 +9,102 @@ function escribir(params) {
     fs.writeFile('./entradas.json', JSON.stringify(entradas), (err) => {
             if (err) console.log('Error writing file:', err)
         })
-
     
 }
+//Parametro debe ser un objeto con las propiedades Dia,Mes,anio
+function buscarfecha(params) {
+    const porfecha= [];
+    entradas.forEach(element => {
+        if (element.time.Dia == params.Dia && element.time.Mes == params.Mes && element.time.anio == params.anio) {
+            porfecha.push(element); 
+        }
+    });
+    //Retorna un arreglo de las concidencias almacenadas
+    return porfecha
+}
 
+//Parametro debe ser un objeto con las propiedades Dia,Mes,anio
+function promediofecha(params) {
+    const fecha = buscarfecha(params)
+    var retorno;
+    fecha.forEach(element => {
+        retorno.humedad += element.humedad
+        retorno.tempertura += element.tempertura
+        retorno.presion += element.presion
+        retorno.luz += element.luz
+        retorno.viento += element.viento
+    });
+    retorno.humedad = retorno.humedad/fecha.length
+    retorno.tempertura = retorno.tempertura/fecha.length
+    retorno.presion = retorno.presion/fecha.length
+    retorno.luz = retorno.luz/fecha.length
+    retorno.viento = retorno.viento/fecha.length
+//Retorna un objeto con el promedio de las 5 propiedades en analisis 
+    return retorno
+}
+//Parametro debe ser un objeto con las propiedades Dia,Mes,anio,hora
+function buscarhora(params) {
+    const porhora= [];
+    entradas.forEach(element => {
+        if (element.time.Dia == params.Dia && element.time.Mes == params.Mes && element.time.anio == params.anio && element.time.hora == params.hora) {
+            porhora.push(element); 
+        }
+    });
+    //Retorna un arreglo de las concidencias almacenadas
+    return porhora
+}
+
+//Parametro debe ser un objeto con las propiedades Dia,Mes,anio,hora
+function promediohora(params) {
+    const fecha = buscarhora(params)
+    var retorno;
+    fecha.forEach(element => {
+        retorno.humedad += element.humedad
+        retorno.tempertura += element.tempertura
+        retorno.presion += element.presion
+        retorno.luz += element.luz
+        retorno.viento += element.viento
+    });
+    retorno.humedad = retorno.humedad/fecha.length
+    retorno.tempertura = retorno.tempertura/fecha.length
+    retorno.presion = retorno.presion/fecha.length
+    retorno.luz = retorno.luz/fecha.length
+    retorno.viento = retorno.viento/fecha.length
+//Retorna un objeto con el promedio de las 5 propiedades en analisis 
+    return retorno
+}
+//Dos parametros de fecha deben ser objetos con las propiedades Dia,Mes,anio
+function buscarrango(inicial,final) {
+    const porrango= [];
+    entradas.forEach(element => {
+        if ((element.time.Dia >= inicial.Dia && element.time.Dia <= final.Dia) && (element.time.Mes >= inicial.Mes && element.time.Mes <= final.mes)&& (element.time.anio >= inicial.anio && element.time.anio <= final.anio )) {
+            porrango.push(element); 
+        }
+    });
+    //Retorna un arreglo de las concidencias almacenadas
+    return porrango
+}
+
+//Dos parametros de fecha deben ser objetos con las propiedades Dia,Mes,anio
+function promediorango(inicial,final) {
+    const fecha = buscarrango(inicial,final)
+    var retorno;
+    fecha.forEach(element => {
+        retorno.humedad += element.humedad
+        retorno.tempertura += element.tempertura
+        retorno.presion += element.presion
+        retorno.luz += element.luz
+        retorno.viento += element.viento
+    });
+    retorno.humedad = retorno.humedad/fecha.length
+    retorno.tempertura = retorno.tempertura/fecha.length
+    retorno.presion = retorno.presion/fecha.length
+    retorno.luz = retorno.luz/fecha.length
+    retorno.viento = retorno.viento/fecha.length
+//Retorna un objeto con el promedio de las 5 propiedades en analisis 
+    return retorno
+}
 
 module.exports= {
-    escribir
+    escribir,buscarfecha,promediofecha,buscarhora,promediohora,buscarrango,promediorango
 }
